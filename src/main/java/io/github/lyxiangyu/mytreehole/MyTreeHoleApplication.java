6c132a9b2f196840a7de2f2dd14f783a1adc29de
xiangyu2233 <759803588@qq.com>
2024-12-21 15:21:46 +0800

    初次提交：包含源码和配置文件

---
+++ src/main/java/io/github/lyxiangyu/mytreehole/MyTreeHoleApplication.java
@@ -0,0 +1,13 @@
+package io.github.lyxiangyu.mytreehole;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyTreeHoleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyTreeHoleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/lyxiangyu/mytreehole/MyTreeHoleApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.lyxiangyu.mytreehole;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyTreeHoleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
